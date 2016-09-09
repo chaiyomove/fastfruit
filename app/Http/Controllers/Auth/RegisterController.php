0527fd087ea89ed7775f9d3b1cf82370c86c02e4
chaiyomove/fastfruit <chaiyo_move83@hotmail.com>
2016-09-09 23:43:58 +0700

    change default auth model ;ready to register and login

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\User;
+use App\Users;
 use Validator;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\RegistersUsers;
@@ -58,12 +58,12 @@ class RegisterController extends Controller
      * Create a new user instance after a valid registration.
      *
      * @param  array  $data
-     * @return User
+     * @return Users
      */
     protected function create(array $data)
     {
-        return User::create([
-            'name' => $data['name'],
+        return Users::create([
+            'firstName' => $data['name'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
         ]);
--- app/Users.php
+++ app/Users.php
@@ -2,9 +2,23 @@
 
 namespace App;
 
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
 use Illuminate\Database\Eloquent\Model;
 
-class Users extends Model
+class Users extends Authenticatable
 {
-    //
+	use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'firstName', 'email', 'password',
+    ];
+
+    // protected $primaryKey = 'idUser';
 }
