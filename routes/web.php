b2302140bf955d226ffa3e020ff17c7eba4339f8
KULAPAT-PC\kulapat <amkulapat@gmail.com>
2016-09-11 00:14:56 +0700

    routes

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,31 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index');
+Route::get('/', 'HomeController@index');
+
+Route::get('/orchards', 'FastFruitController@orchards');
+Route::get('/products', 'FastFruitController@products');
+Route::get('/matching', 'FastFruitController@matching');
+Route::get('/contactus', 'FastFruitController@contactUs');
+Route::get('/products/{id}', 'FastFruitController@productDetail');
+Route::get('/chat', 'FastFruitController@chat');
+Route::get('/orchards/{id}', 'FastFruitController@orchardDetail');
+Route::get('/userprofile/{id}', 'FastFruitController@userProfile');
+Route::get('/addorchard', 'FastFruitController@addOrchard');
+Route::get('/checkgap', 'FastFruitController@checkGap');
+Route::get('/updateorchard', 'FastFruitController@updateOrchard');
+Route::get('/updateuserprofile', 'FastFruitController@updateUserprofile');
+Route::get('user/orchard', 'FastFruitController@userOrchard');
+Route::get('user/product', 'FastFruitController@userProduct');
+Route::get('user/product/{id}', 'FastFruitController@userProductDetail');
+Route::get('user/addproduct', 'FastFruitController@userAddProduct');
+Route::get('user/matching', 'FastFruitController@userMatching');
+Route::get('user/addadmin', 'FastFruitController@userAddadmin');
+
+
