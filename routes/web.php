3119a26c549893a2f1bf1ffb26d5361f5e814891
chaiyomove/fastfruit <chaiyo_move83@hotmail.com>
2016-09-11 01:50:44 +0700

    แก้ route logout แบบลักไก่

--- routes/web.php
+++ routes/web.php
@@ -14,23 +14,24 @@
 // Route::get('/', function () {
 //     return view('welcome');
 // });
-
+Route::get('logout', 'Auth\LoginController@logout');
 Auth::routes();
 
 Route::get('/', 'HomeController@index');
+Route::get('home', 'HomeController@index');
 
-Route::get('/orchards', 'FastFruitController@orchards');
-Route::get('/products', 'FastFruitController@products');
-Route::get('/matching', 'FastFruitController@matching');
-Route::get('/contactus', 'FastFruitController@contactUs');
-Route::get('/products/{id}', 'FastFruitController@productDetail');
-Route::get('/chat', 'FastFruitController@chat');
-Route::get('/orchards/{id}', 'FastFruitController@orchardDetail');
-Route::get('/userprofile/{id}', 'FastFruitController@userProfile');
-Route::get('/addorchard', 'FastFruitController@addOrchard');
-Route::get('/checkgap', 'FastFruitController@checkGap');
-Route::get('/updateorchard', 'FastFruitController@updateOrchard');
-Route::get('/updateuserprofile', 'FastFruitController@updateUserprofile');
+Route::get('orchards', 'FastFruitController@orchards');
+Route::get('products', 'FastFruitController@products');
+Route::get('matching', 'FastFruitController@matching');
+Route::get('contactus', 'FastFruitController@contactUs');
+Route::get('products/{id}', 'FastFruitController@productDetail');
+Route::get('chat', 'FastFruitController@chat');
+Route::get('orchards/{id}', 'FastFruitController@orchardDetail');
+Route::get('userprofile/{id}', 'FastFruitController@userProfile');
+Route::get('addorchard', 'FastFruitController@addOrchard');
+Route::get('checkgap', 'FastFruitController@checkGap');
+Route::get('updateorchard', 'FastFruitController@updateOrchard');
+Route::get('updateuserprofile', 'FastFruitController@updateUserprofile');
 Route::get('userorchard', 'FastFruitController@userOrchard');
 Route::get('userproduct', 'FastFruitController@userProduct');
 Route::get('userproduct/{id}', 'FastFruitController@userProductDetail');
