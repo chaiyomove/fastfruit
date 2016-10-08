bb1929fa3daa71e37e7f20f54967bb5fcdc7b0f9
Mookk <sirijanya.r@gmail.com>
2016-10-08 16:23:04 +0700

    แก้หน้า orcharddetail productdetail

--- resources/views/OrchardDetail.blade.php
+++ resources/views/OrchardDetail.blade.php
@@ -39,7 +39,7 @@
 					<h3>{{$orchard->nameOrchard}}</h3>
 					<div id="owl-demo" class="owl-carousel">
 						<div class="item">		
-							<div class="testmonial-text" style="width: 97.5%; margin: auto; ">
+							<div class="testmonial-text" style="width: 55%; margin: auto; float: left;">
 								<div class="carousel slide" id="carousel-824332" 	>
 									<ol class="carousel-indicators" >
 										<li class="active" data-slide-to="0" data-target="#carousel-824332">
@@ -51,13 +51,13 @@
 									</ol>
 								<div class="carousel-inner">
 									<div class="item active">
-										<img src="{{$orchard->picture}}" style="height: 350px; width: 860px;" />
+										<img src="{{$orchard->picture}}" style="height: 300px; width: 850px;" />
 									</div>
 									<div class="item">
-										<img src="{{$orchard->picture}}" style="height: 350px; width: 860px;" />										
+										<img src="{{$orchard->picture}}" style="height: 300px; width: 850px;" />										
 									</div>
 									<div class="item">
-										<img src="{{$orchard->picture}}" style="height: 350px; width: 860px;" />
+										<img src="{{$orchard->picture}}" style="height: 300px; width: 850px;" />
 									</div>
 								</div> 
 								<a class="left carousel-control" href="#carousel-824332" data-slide="prev">
@@ -65,11 +65,49 @@
 								<a class="right carousel-control" href="#carousel-824332" data-slide="next">
 								<span class="glyphicon glyphicon-chevron-right"></span></a>
 							</div>
+							<div style="margin-top: 15px">
+							<table>
+								<tr>
+									<td>
+										<button type="button" class="btn btn-info btn-sm"> 
+											<h5>
+											<i class="glyphicon glyphicon-user"> </i>
+											<i class="glyphicon glyphicon-comment"> </i>&nbsp;&nbsp;แชท
+											</h5>
+							            </button>
+									</td>
+									<td>
+										<a href="ProductOfOrchard.html">
+											<button type="button" class="btn btn-danger btn-sm" > 
+												<h5>
+												<i class="glyphicon glyphicon-eye-open">&nbsp;ผลผลิต</i>
+												</h5>
+							                </button>														
+										</a>
+									</td>
+									<td>
+										<button type="button" class="btn btn-success btn-sm"> 
+									        <h5>
+										    <i class="glyphicon glyphicon-ok">&nbsp;ติดตาม</i>
+										    </h5>
+					                    </button>
+									</td>
+									<td>
+										<button type="button" class="btn btn-warning btn-sm"> 
+									        <h5>
+										    <i class="glyphicon glyphicon-globe">&nbsp;แผนที่</i>
+										    </h5>
+					                    </button>
+									</td>
+								</tr>
+							</table>
+								
+					            
+							</div>
 						</div>
-							<br>
 
 								<!--Gentelela_table-->
-									<div class="col-md-12 col-sm-12 col-xs-12">
+									<div class="col-md-12 col-sm-12 col-xs-12" style="width: 45%; margin: auto; float: right;">
 									    <div class="x_panel">
 									    <div class="x_title">
 									        <h4>ข้อมูล {{$orchard->nameOrchard}}</h4>
@@ -80,61 +118,33 @@
 									            <table class="table table-striped">
 									                <tbody>
 									                <tr>
-									                    <th scope="row" width="15%">รายละเอียด</th>
+									                    <th scope="row" width="40%">รายละเอียด</th>
 									                    <td colspan="2" align="left">
 									                    {{$orchard->description}}	
 									                    </td>
 									                    <td></td>
 									                </tr>
 									                <tr>
-									                    <th scope="row" width="15%">ที่อยู่</th>
+									                    <th scope="row" width="40%">ที่อยู่</th>
 									                    <td colspan="2" align="left">{{$orchard->address}}               	
 									                    </td>
 									                    <td></td>
 									                </tr>
 									                <tr>
-									                    <th scope="row" width="15%">จังหวัด</th>
+									                    <th scope="row" width="40%">จังหวัด</th>
 									                    <td colspan="2" align="left">{{$orchard->province->provinceName}}</td>
 									                    <td></td>
 									                </tr>
 									                <tr>
-									                    <th scope="row">พื้นที่</th>
+									                    <th scope="row" width="40%">พื้นที่</th>
 									                    <td align="left" width="5%">500</td>
 									                    <td align="left">ไร่</td>
 									                </tr>
 									                <tr>
-									                    <th scope="row">ติดต่อผู้ขาย</th>
+									                    <th scope="row" width="40%">ติดต่อผู้ขาย</th>
 									                    <td colspan="2" align="left">{{$orchard->phone}}</td>
 									                    <td></td>
-									                </tr>									                
-									                <tr>
-									                    <td>
-									                        <button type="button" class="btn btn-info btn-sm" style="margin-left: -15px"> 
-									                            <h5>
-										                            <i class="glyphicon glyphicon-user"> </i>
-											                        <i class="glyphicon glyphicon-comment"> </i>&nbsp;&nbsp;แชท
-										                        </h5>
-					                            			</button>
-
-
-					                            			<a href="ProductOfOrchard.html">
-									                			<button type="button" class="btn btn-danger btn-sm" style="margin-left: -10px; margin-top:10px"> 
-										                            <h5>
-											                            <i class="glyphicon glyphicon-eye-open">&nbsp;ผลผลิต</i>
-											                        </h5>
-					                            				</button>														
-															</a>
-									                    </td>
-									                    <td>
-									                    	<button type="button" class="btn btn-success btn-sm" style="margin-left: -15px"> 
-									                            <h5>
-										                            <i class="glyphicon glyphicon-ok">&nbsp;ติดตาม</i>
-										                        </h5>
-					                            			</button>
-									                    </td>
-									                    <td></td>
-									                </tr>
-									                
+									                </tr>							                
 									                </tbody>
 									            </table>
 
--- resources/views/ProductDetail.blade.php
+++ resources/views/ProductDetail.blade.php
@@ -39,7 +39,7 @@
 					<h3>ข้อมูลผลผลิต</h3>
 					<div id="owl-demo" class="owl-carousel">
 						<div class="item">		
-							<div style="width: 97.5%; margin: auto; ">
+							<div style="width: 55%; margin: auto; float: left;">
 								<div class="carousel slide" id="carousel-824332" 	>
 									<ol class="carousel-indicators" >
 										<li class="active" data-slide-to="0" data-target="#carousel-824332">
@@ -51,13 +51,13 @@
 									</ol>
 								<div class="carousel-inner">
 									<div class="item active">
-										<img src="{{$product->picture}}" style="height: 350px; width: 860px;" />
+										<img src="{{$product->picture}}" style="height: 300px; width: 850px;" />
 									</div>
 									<div class="item">
-										<img src="{{$product->picture}}" style="height: 350px; width: 860px;" />										
+										<img src="{{$product->picture}}" style="height: 300px; width: 850px;" />										
 									</div>
 									<div class="item">
-										<img src="{{$product->picture}}" style="height: 350px; width: 860px;" />
+										<img src="{{$product->picture}}" style="height: 300px; width: 850px;" />
 									</div>
 								</div> 
 								<a class="left carousel-control" href="#carousel-824332" data-slide="prev">
@@ -65,11 +65,49 @@
 								<a class="right carousel-control" href="#carousel-824332" data-slide="next">
 								<span class="glyphicon glyphicon-chevron-right"></span></a>
 							</div>
+							<div style="margin-top: 15px">
+							<table>
+								<tr>
+								<form method="GET" action="{{url('/orcharddetail')}}">
+									{{ csrf_field() }}
+						            <input type="hidden" name="idProductSprint" value="{{$product->idProductSprint}}">
+
+									<td>
+										<button type="button" class="btn btn-info btn-sm"> 
+											<h5>
+											<i class="glyphicon glyphicon-user"> </i>
+											<i class="glyphicon glyphicon-comment"> </i>&nbsp;&nbsp;แชท
+											</h5>
+							            </button>
+									</td>
+									<td>
+										{{-- <a href="OrchardDetail-bf.html"> --}}
+								                			
+								            <input type="hidden" name="idOrchard" value="{{$product->orchardPlot->orchard->idOrchard}}">	
+												<button type="submit" class="btn btn-danger btn-sm">
+													<h5>
+													<span class="glyphicon glyphicon-eye-open">&nbsp;ชมสวน</span>
+													</h5>
+												</button>														
+										{{-- </a> --}}
+									</td>
+									<td>
+								        <button type="button" class="btn btn-warning btn-sm"> 
+								            <h5>
+									        <i class="glyphicon glyphicon-star"> </i>
+									        </h5>
+				                        </button>
+								    </td>
+								</form>
+								</tr>
+							</table>
+								
+					            
+							</div>
 						</div>
-							<br>
 
 								<!--Gentelela_table-->
-									<div class="col-md-12 col-sm-12 col-xs-12">
+									<div class="col-md-12 col-sm-12 col-xs-12" style="width: 45%; margin: auto; float: right;">
 						                <div class="x_panel">
 						                  <div class="x_title">
 						                    <h4>{{$product->orchardPlot->fruitSpecie->specieName}} จาก {{$product->orchardPlot->orchard->nameOrchard}}</h4>
@@ -81,27 +119,27 @@
 						                    <table class="table table-striped">
 						                      <tbody>
 						                        <tr>
-						                          <th scope="row" width="15%">รายละเอียด</th>
+						                          <th scope="row" width="40%">รายละเอียด</th>
 						                          <td colspan="2" align="left">{{$product->description}}	</td>
 						                          <td></td>
 						                        </tr>
 						                        <tr>
-						                          <th scope="row">จำนวน</th>
+						                          <th scope="row" width="40%">จำนวน</th>
 						                          <td align="left" width="5%">{{$product->fruitNum}}</td>
 						                          <td align="left">กิโลกรัม</td>
 						                        </tr>
 						                        <tr>
-						                          <th scope="row">ติดต่อผู้ขาย</th>
+						                          <th scope="row" width="40%">ติดต่อผู้ขาย</th>
 						                          <td colspan="2" align="left">{{$product->orchardPlot->orchard->phone}}</td>
 						                          <td></td>
 						                        </tr>
 						                        <tr>
-						                          <th scope="row">ตั้งแต่วันที่</th>
+						                          <th scope="row" width="40%">ตั้งแต่วันที่</th>
 						                          <td colspan="2" align="left">{{$product->startDate}}</td>
 						                          <td></td>
 						                        </tr>
 						                        <tr>
-						                          <th scope="row">ถึงวันที่</th>
+						                          <th scope="row" width="40%">ถึงวันที่</th>
 						                          <td colspan="2" align="left" padding="20px">{{$product->endDate}}</td>
 						                          <td></td>
 						                        </tr>
@@ -109,32 +147,7 @@
 						                        	<form method="GET" action="{{url('/orcharddetail')}}">
 													{{ csrf_field() }}
 						                       		<input type="hidden" name="idProductSprint" value="{{$product->idProductSprint}}">
-								                    <td>
-								                        <button type="button" class="btn btn-info btn-sm" style="margin-left: -15px"> 
-								                            <h5>
-									                            <i class="glyphicon glyphicon-user"> </i>
-										                        <i class="glyphicon glyphicon-comment"> </i>&nbsp;&nbsp;แชท
-									                        </h5>
-				                            			</button>
 
-
-				                            			{{-- <a href="OrchardDetail-bf.html"> --}}
-								                			
-								                			<input type="hidden" name="idOrchard" value="{{$product->orchardPlot->orchard->idOrchard}}">	
-															<button type="submit" class="btn btn-danger btn-sm" style="margin-left: -15px; margin-top:10px">
-																<h5>
-																	<span class="glyphicon glyphicon-eye-open">&nbsp;ชมสวน</span>
-																</h5>
-															</button>														
-														{{-- </a> --}}
-								                    </td>
-								                    <td>
-								                    	<button type="button" class="btn btn-warning btn-sm" style="margin-left: -15px"> 
-								                            <h5>
-									                            <i class="glyphicon glyphicon-star"> </i>
-									                        </h5>
-				                            			</button>
-								                    </td>
 								                    <td>
 								                    	@if ($product->orchardPlot->idPlotStatus == 1)
 								                    		<img src="../images/gap.png" style="float:right;height:48px;width:50px">
