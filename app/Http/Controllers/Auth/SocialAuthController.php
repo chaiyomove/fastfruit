010a61a2fcbbddb8bce860062b51aadcd9a14422
Sumet Benjawan <chaiyo_move83@hotmail.com>
2016-11-14 20:24:14 +0700

    require laravel/socialite

---
+++ app/Http/Controllers/Auth/SocialAuthController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use Illuminate\Http\Request;
+
+use App\Http\Controllers\Controller;
+
+use App\Http\Requests;
+
+use Socialite;
+
+class SocialAuthController extends Controller
+{
+	/**
+     * Redirect the user to the Facebook authentication page.
+     *
+     * @return Response
+     */
+    public function redirectToProvider()
+    {
+        return Socialite::driver('facebook')->redirect();
+    }
+
+    /**
+     * Obtain the user information from Facebook.
+     *
+     * @return Response
+     */
+    public function handleProviderCallback()
+    {
+        $user = Socialite::driver('facebook')->user(); 
+
+        // $user->token;
+    }
+}
