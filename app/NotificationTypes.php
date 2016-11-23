7e2ca4942a25c5b495815a4aed7af7d921637295
KULAPAT-PC\kulapat <amkulapat@gmail.com>
2016-11-23 21:30:06 +0700

    no message

--- app/NotificationTypes.php
+++ app/NotificationTypes.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class NotificationTypes extends Model
 {
-    //
+     protected $primaryKey = 'id';
+     protected $fillable = [
+        'description'
+    ];
+    protected $table = 'NotificationTypes';
+
+     public function notifications()
+    {
+    	return $this->hasMany('App\Notifications','idNotiType');
+    }
 }
--- app/Notifications.php
+++ app/Notifications.php
@@ -6,5 +6,29 @@ use Illuminate\Database\Eloquent\Model;
 
 class Notifications extends Model
 {
-    //
+	 protected $primaryKey = 'id';
+     protected $fillable = [
+        'notiType','seenStatus','idUser','idOrchard','idNotiType','idProductSprint'
+    ];
+    protected $table = 'notifications';
+
+    public function orchard()
+    {
+    	return $this->belongsTo('App\Orchards','idOrchard');
+    }
+
+    public function user()
+    {
+    	return $this->belongsTo('App\Users','idUser');
+    }
+
+    public function notificationTyes()
+    {
+    	return $this->belongsTo('App\NotificationTypes','idNotiType');
+    }
+
+    public function productSprint()
+    {
+    	return $this->belongsTo('App\Product_sprints','idProductSprint');
+    }
 }
